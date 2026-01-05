6914fdb0c6e53d55f04fcb381f2d6c9b9caa873a
Werner Pyke <wernerpyke@gmail.com>
2026-01-05 13:51:08 +0200

    Fixed splatter

--- pyke_pyxel/effects/_splatter.py
+++ pyke_pyxel/effects/_splatter.py
@@ -7,31 +7,41 @@ class _SplatterEffect(_Effect):
         super().__init__(None)
         self._colour = colour
         self._position = position
-        self._iteration = 0
+        self._frame = 0
 
-        self._origin_x = position.mid_x
-        self._origin_y = position.max_y - 2
+        self._x = position.mid_x
+        self._y = position.mid_y
 
     def _do(self):
-        # Assume 60 FPS
-        if self._iteration < 5:
-            pyxel.pset(self._origin_x-1, self._origin_y, self._colour)
-            pyxel.pset(self._origin_x, self._origin_y, self._colour)
-            pyxel.pset(self._origin_x+1, self._origin_y, self._colour)
-        elif self._iteration < 10:
-            pyxel.pset(self._origin_x-1, self._origin_y+1, self._colour)
-            pyxel.pset(self._origin_x, self._origin_y, self._colour)
-            pyxel.pset(self._origin_x+1, self._origin_y+1, self._colour)
-        elif self._iteration < 20:
-            pyxel.pset(self._origin_x-2, self._origin_y+2, self._colour)
-            pyxel.pset(self._origin_x, self._origin_y, self._colour)
-            pyxel.pset(self._origin_x+2, self._origin_y+2, self._colour)
-        # elif self._iteration < 40:
-        #    pyxel.pset(self._origin_x-2, self._origin_y+2, self._colour)
-        #    pyxel.pset(self._origin_x, self._origin_y, self._colour)
-        #    pyxel.pset(self._origin_x+2, self._origin_y+2, self._colour)
-        else:
-            self._complete()
-            return
+        # TODO - the below assumes 60 FPS
+        frame = self._frame
 
-        self._iteration += 1
+        if frame <= 10:
+            # Centre
+            pyxel.pset(self._x-1, self._y, self._colour)
+            pyxel.pset(self._x-1, self._y+1, self._colour)
+            pyxel.pset(self._x, self._y, self._colour)
+            pyxel.pset(self._x, self._y+1, self._colour)
+            
+        if (frame > 5) and (frame <= 10):
+            pyxel.pset(self._x-1, self._y-2, self._colour)  # Top
+            pyxel.pset(self._x+1, self._y+3, self._colour)  # Bottom
+            pyxel.pset(self._x+3, self._y, self._colour)    # Right
+            pyxel.pset(self._x-2, self._y+1, self._colour)  # Left
+
+        if (frame > 10) and (frame <= 20):
+            pyxel.pset(self._x-1, self._y-4, self._colour)  # N
+            pyxel.pset(self._x+4, self._y, self._colour)    # E
+            pyxel.pset(self._x+1, self._y+4, self._colour)  # S
+            pyxel.pset(self._x-4, self._y, self._colour)    # W
+
+        if (frame > 10) and (frame <= 40):
+            pyxel.pset(self._x+4, self._y-3, self._colour)  # NE
+            pyxel.pset(self._x+3, self._y+2, self._colour)  # SE
+            pyxel.pset(self._x, self._y+2, self._colour)    # SW
+            pyxel.pset(self._x-2, self._y-2, self._colour)  # NW
+
+        if frame > 40:
+            self._complete()
+
+        self._frame += 1
